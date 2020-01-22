945b1a97a69da1c91b1dfbba9a126c19d5584522
yonasyifter <yonasyifter@gmail.com>
2020-01-22 16:17:10 -0500

    home navigation with  a card

--- website/home/admin.py
+++ website/home/admin.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from tinymce.widgets import TinyMCE
+from django.db import models
+from .models import Python
 
 # Register your models here.
+class pyAdmin(admin.ModelAdmin):
+    fieldsets = [('Title and Date',{'fields':['title','date']}),
+                 ('Content Block',{'fields':['video','explain']})]
+    formfield_overrides = {
+        models.TextField: {'widget': TinyMCE()},
+        }
+
+admin.site.register(Python, pyAdmin)
--- website/home/models.py
+++ website/home/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Python(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    video = models.TextField()
+    explain = models.TextField()
+
+def __str__(self):
+    return title
